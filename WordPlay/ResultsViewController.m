cd003fdf2d634a830d24e3f9e78c3a9d61676e9b
Valak12 <Kohnechi@gmail.com>
2014-10-21 20:08:16 -0700

    FINALLY DONE

--- WordPlay/ResultsViewController.m
+++ WordPlay/ResultsViewController.m
@@ -18,7 +18,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.resultsTextView.text = [NSMutableString stringWithFormat:@"Hi my name is %@, I am really %@ %@ %@",self.name, self.adjective, self.noun, self.adverb];
+    
+    NSString *textString = [NSString stringWithFormat:@"Hi my name is %@, I am really %@ %@ %@",self.name, self.adjective, self.noun, self.adverb];
+    
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:textString];
+    [attributedString setAttributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:[UIFont systemFontSize]]} range:[textString rangeOfString:self.name]];
+    [attributedString setAttributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:[UIFont systemFontSize]]} range:[textString rangeOfString:self.adjective]];
+    [attributedString setAttributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:[UIFont systemFontSize]]} range:[textString rangeOfString:self.noun]];
+    [attributedString setAttributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:[UIFont systemFontSize]]} range:[textString rangeOfString:self.adverb]];
+    self.resultsTextView.attributedText = attributedString;
+
+    
+    
+ 
+
 }
 
 
